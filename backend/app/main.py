965e564e77b7d8c122490435b15f1c569996ddea
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-01-31 02:39:51 +0530

    Update main.py
    final commit before deploying

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import auth, projects
+from app.api import auth, projects, users 
 from app.db.session import engine, Base
 
-# Create tables logic
 async def init_db():
     async with engine.begin() as conn:
-        # This actually creates the tables in the database
         await conn.run_sync(Base.metadata.create_all)
 
 app = FastAPI(
     title="Jira Clone API",
 )
 
-origins = [
-    "http://localhost:5173",           # Allows your local laptop
-    "https://fast-api-and-react-git-jiraclone-yash-kadams-projects-4187f088.vercel.app/",   
-    "*"                                # Failsafe: Allows everyone (good for testing)
-]
-# CORS Configuration
-# We use ["*"] to allow YOUR Vercel frontend to connect without issues.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -32,9 +23,9 @@ app.add_middleware(
 async def on_startup():
     await init_db()
 
-# Include your API routes
 app.include_router(auth.router)
 app.include_router(projects.router)
+app.include_router(users.router) #
 
 @app.get("/",)
 async def root():
