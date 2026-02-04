7cde89b081fac3db62062571372d8c40a4b96fa1
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-04 12:48:03 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,6 +14,7 @@ export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (data) => {
+    console.log("Form Data:", data);
     setIsLoading(true);
     setServerError('');
 
