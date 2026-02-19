c57d5dfe493ccb1fe67de767dbb27d3c5eee49b6
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 20:17:24 +0530

    Update logic.py

--- backend/services/logic.py
+++ backend/services/logic.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from models.tables import AuditLog, Document
 import uuid
-from datetime import datetime  
+from datetime import datetime, timezone 
 
 def log_action(db: Session, doc_id: int, user_id: int, action: str, ip: str = "127.0.0.1"):
     new_log = AuditLog(
@@ -9,7 +9,7 @@ def log_action(db: Session, doc_id: int, user_id: int, action: str, ip: str = "1
         performed_by = user_id,
         action = action,
         ip_address = ip,
-        timestamp = datetime.utcnow()
+        timestamp = datetime.now(timezone.utc)
     )
     db.add(new_log)
     db.commit()
@@ -22,6 +22,5 @@ def generate_public_link(db: Session, doc_id: int):
     share_token = str(uuid.uuid4())
     doc.share_token = share_token 
     db.commit()
-    
-    public_url = f"http://localhost:5173/public/view/{share_token}"
+    public_url = f"https://fast-api-and-react.vercel.app/public/view/{share_token}"
     return {"share_url": public_url}
