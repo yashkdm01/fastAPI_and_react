559e2f41f959da427d8eb18d2f6f6e47c321fac9
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 20:32:31 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,8 +2,10 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoute = () => {
   const token = localStorage.getItem("token");
+
   if (!token) {
     return <Navigate to="/login" replace />;
   }
+
   return <Outlet />;
 };
