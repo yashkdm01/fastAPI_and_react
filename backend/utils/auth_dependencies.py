dc176aff282bae7ddb0e271e4b1c70b432514936
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 16:52:32 +0530

    Update auth_dependencies.py

--- backend/utils/auth_dependencies.py
+++ backend/utils/auth_dependencies.py
@@ -8,7 +8,7 @@ from utils.security import SECRET_KEY, ALGORITHM
 
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"},)
