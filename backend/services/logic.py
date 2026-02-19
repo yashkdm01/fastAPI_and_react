73d25e380504837626fe8410cd77ffabad18229d
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 21:26:07 +0530

    Update logic.py

--- backend/services/logic.py
+++ backend/services/logic.py
@@ -22,5 +22,5 @@ def generate_public_link(db: Session, doc_id: int):
     share_token = str(uuid.uuid4())
     doc.share_token = share_token 
     db.commit()
-    public_url = f"https://fast-api-and-react.vercel.app/public/view/{share_token}"
-    return {"share_url": public_url}
+    
+    return {"share_token": share_token}
