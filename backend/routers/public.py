bb396242da2c032f2acaee2e01ea2d67b6a34b62
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 20:14:24 +0530

    Update public.py

--- backend/routers/public.py
+++ backend/routers/public.py
@@ -1,18 +1,20 @@
 import os
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from database import get_db
 from models.tables import Document
 
-router = APIRouter(prefix="/public", tags=["Public Access"])
+router = APIRouter(tags=["Public Access"])
 
 @router.get("/view/{token}")
-def get_public_document(token: str, db: Session = Depends(get_db)):
+def get_public_document(token: str, request: Request, db: Session = Depends(get_db)):
     doc = db.query(Document).filter(Document.share_token == token).first()
     if not doc:
         raise HTTPException(status_code=404, detail="Link invalid")
+    
     filename = os.path.basename(doc.file_url)
-    clean_url = f"http://localhost:8000/uploads/{filename}"
+    base_url = str(request.base_url).rstrip("/")
+    clean_url = f"{base_url}/uploads/{filename}"
         
     return {
         "title": doc.title,
