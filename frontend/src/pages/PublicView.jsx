3253d30415a7eb5538fe7cc38d7eec01e92ecadf
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 16:18:14 +0530

    Update PublicView.jsx

--- frontend/src/pages/PublicView.jsx
+++ frontend/src/pages/PublicView.jsx
@@ -7,7 +7,7 @@ import { Download, ShieldCheck } from "lucide-react";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-const publicApi = axios.create({ baseURL: "fastapiandreact-production.up.railway.app" });
+const publicApi = axios.create({ baseURL: "https://fastapiandreact-production.up.railway.app" });
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export const PublicView = () => {
