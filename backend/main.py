7dd4ad807cfe698651ec11f1f8e23088dfaeadc9
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 14:55:18 +0530

    Update main.py
    added vercel link to main.py

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Signature App API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                  "https://signature-app.vercel.app"], 
+                  "https://fast-api-and-react.vercel.app",], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
