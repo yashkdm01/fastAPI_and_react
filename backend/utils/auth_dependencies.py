bc99a6647956d8694b5a982275e0dc2a3f9de8c6
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 20:41:16 +0530

    Update auth_dependencies.py

--- backend/utils/auth_dependencies.py
+++ backend/utils/auth_dependencies.py
@@ -6,7 +6,7 @@ from database import get_db
 from models.tables import User
 from utils.security import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
