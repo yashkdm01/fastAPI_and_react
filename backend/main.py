29169f50227c76514178b19a040061732e9984d4
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2026-02-19 16:39:04 +0530

    Update main.py
    updates cors

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,7 @@ app = FastAPI(title="Signature App API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                  "https://fast-api-and-react.vercel.app",
-                  "https://fast-api-and-react.vercel.app/"], 
+                  "https://fast-api-and-react.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
